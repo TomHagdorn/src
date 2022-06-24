a00811db1cd962c0779d8e9b0d7174149f1a1cb1
Tom Hagdorn <T.Hagdorn@wzl.rwth-aachen.de>
2022-06-24 13:43:52 +0200

    repair master after merge

--- my_robot_slam/setup.py
+++ my_robot_slam/setup.py
@@ -16,15 +16,11 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-<<<<<<< HEAD
-
-=======
         (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
         (os.path.join('share', package_name, 'urdf'), glob('urdf/*.xacro')),
         (os.path.join('share', package_name, 'meshes'), glob('meshes/*.stl')),
         (os.path.join('share', package_name, 'maps'), glob('maps/*.yaml')),
         (os.path.join('share', package_name, 'maps'), glob('meshes/*.pgm')),
->>>>>>> luigi_initial_test
     ],
     install_requires=['setuptools'],
     zip_safe=True,
