8f9290436ee4e8a851d01ddd1f81159d51113dae
Tom Hagdorn <T.Hagdorn@wzl.rwth-aachen.de>
2022-06-24 10:44:28 +0200

    tb hardware: moved launch file

--- tb3_hardware/setup.py
+++ tb3_hardware/setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup
-
+from glob import glob 
 package_name = 'tb3_hardware'
 
 setup(
@@ -10,6 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
