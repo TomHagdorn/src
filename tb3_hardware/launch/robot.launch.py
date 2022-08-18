ed7f4fc2afca146b7c956065daa623ebc8a236ea
Tom Hagdorn <T.Hagdorn@wzl.rwth-aachen.de>
2022-08-18 11:19:54 +0200

    Master: Changed Location of robot launch to tb3_hardware

---
+++ tb3_hardware/launch/robot.launch.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
+
+    usb_port = LaunchConfiguration('usb_port', default='/dev/ttyACM0')
+
+    tb3_param_dir = LaunchConfiguration(
+        'tb3_param_dir',
+        default=os.path.join(
+            get_package_share_directory('turtlebot3_bringup'),
+            'param',
+            TURTLEBOT3_MODEL + '.yaml'))
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'usb_port',
+            default_value=usb_port,
+            description='Connected USB port with OpenCR'),
+
+        DeclareLaunchArgument(
+            'tb3_param_dir',
+            default_value=tb3_param_dir,
+            description='Full path to turtlebot3 parameter file to load'),
+
+        Node(
+            package='turtlebot3_node',
+            node_executable='turtlebot3_ros',
+            parameters=[tb3_param_dir],
+            arguments=['-i', usb_port],
+            output='screen'),
+    ])
